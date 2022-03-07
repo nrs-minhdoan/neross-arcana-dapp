168a7f27f9f20df5421f3b7eb729da8f96e85160
doantruongminh <doantruongminh@cryptopie-labo.com>
2022-03-07 11:22:32 +0700

    Fix auth

--- src/store/auth/auth.saga.ts
+++ src/store/auth/auth.saga.ts
@@ -1,7 +1,8 @@
-import { call, put, takeLatest } from "@redux-saga/core/effects";
+import { call, put, select, takeLatest } from "@redux-saga/core/effects";
 
 import arcanaNetworkSDK, { LoginType } from "../../sdks/arcanaNetwork";
 import { plainToClass } from "../../utils/classTransformer";
+import { IStoreState } from "../../models/store";
 import { UserInfo } from "../../models/store/auth.model";
 import {
   validateSession,
@@ -12,12 +13,17 @@ import {
 function* handleValidateSession({
   payload,
 }: ReturnType<typeof validateSession>) {
+  const isNewSession: boolean = yield select(
+    (store: IStoreState) => store.auth.isNewSession
+  );
   const isLoggedIn = arcanaNetworkSDK.isLoggedIn;
-  if (isLoggedIn) {
-    payload.callbacks.onSuccess();
-  } else {
-    yield put(destroySession());
-    payload.callbacks.onError();
+  if (!isNewSession) {
+    if (isLoggedIn) {
+      payload.callbacks.onSuccess();
+    } else {
+      yield put(destroySession());
+      payload.callbacks.onError();
+    }
   }
 }
 
@@ -55,6 +61,7 @@ function* handleInitSessionWithGoogle({
 }
 
 function* handleDestroySession() {
+  console.log("logged out: ", "");
   yield call(arcanaNetworkSDK.logout);
 }
 

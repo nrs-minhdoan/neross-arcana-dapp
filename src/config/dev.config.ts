76c7ef1fa606d1670f9c198326ba11eace822999
doantruongminh <doantruongminh@cryptopie-labo.com>
2022-03-07 11:41:58 +0700

    Update sdk

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -1,7 +1,7 @@
 const CONFIG = {
   APP_NAME: "NeroSs DApp",
   APP_URL: "https://neross.netlify.app",
-  APP_ID: "478",
+  APP_ID: "561",
   GOOGLE_CLIENT_ID:
     "713121082738-m96mkqdrl83ptdjn224e46ik8ergep6r.apps.googleusercontent.com",
 };
